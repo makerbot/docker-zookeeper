f2cbc382ff4033a9d77f23a6719b187b8d259d6c
George Shammas <george.shammas@makerbot.com>
2016-03-01 14:52:35 -0500

    zookeeper requires a number as an ID, no letters, lets fix that

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-printf "${ZK_SERVER_NUMBER}" > /var/lib/zookeeper/myid
+function num-from-string() {
+    local out i a
+    for ((i=0;i<${#1};++i)); do
+        printf -v a "%d\n" "'${1:i:1}"
+        out+=$((a%10))
+    done
+    echo "$out"
+}
+
+printf "$(num-from-string ${ZK_SERVER_NUMBER})" > /var/lib/zookeeper/myid
 printf "Starting Zookeeper server $(cat /var/lib/zookeeper/myid)\n"
 /opt/zookeeper/bin/zkServer.sh start-foreground
